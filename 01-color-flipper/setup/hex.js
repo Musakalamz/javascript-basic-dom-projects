32500b54b7388ac2c3f480f918824adcec21d9ae
Musakalamz <ogunsolumusa@gmail.com>
2026-01-02 09:48:02 +0100

    feat: changing background colour dynamically using specified colors and random generated hex colors

---
+++ 01-color-flipper/setup/hex.js
@@ -0,0 +1,20 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+function generateRandomNumbers() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+
+  for (let index = 0; index < 6; index++) {
+    // hexColor += hex[6];
+    hexColor += hex[generateRandomNumbers()];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
